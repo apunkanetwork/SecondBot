ddb2f39fdb48de3dbc6a56170b3e847872e6a15e
apunkanetwork <105333651+apunkanetwork@users.noreply.github.com>
2022-07-16 13:27:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from pyrogram import Client, filters 
-
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton 
+from pyrogram.errors import UserNotParticipant 
 
 API_ID = "6362436"
 API_HASH = "c0e8c637c6ea7c97fce7f4d9622aa023"
@@ -12,10 +13,28 @@ OXYVER = Client(
     bot_token=BOT_TOKEN
 )
 
+force_channel = "Oxyver"
+
 
 @OXYVER.on_message(filters.command("start"))
-async def start_cmd(client, message):
-    await message.reply_text("** If You Want To Know More Contact My Owner @Oxyver_Owner**")
+async def start_cmd(bot, msg):
+    if force_channel:
+        try:
+            user = await bot.get_chat_member(force_channel, msg.from_user.id)
+            if user.status == "kicked out":
+                await msg.reply_text("You Are Banned")
+                return
+        except UserNotParticipant:
+            await msg.reply_text(
+                text="You Are Not A Member Of My Channel",
+                reply_markup=InlineKeyboardMarkup( [[
+                 InlineKeyboardButton("Join Channel 📣", url=f"t.me/{force_channel}")
+                 ]]
+                )
+            )
+
+
+    await msg.reply_text("** If You Want To Know More Contact My Owner @Oxyver_Owner**")
 
 
 print("Bot Started")
