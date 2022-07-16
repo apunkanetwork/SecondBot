3929a2ebb444c871eeb10bb6a54dc8ec3a9c7ae6
apunkanetwork <105333651+apunkanetwork@users.noreply.github.com>
2022-07-16 12:50:58 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+from pyrogram import Client, filters 
+
+
+API_ID = "6362436"
+API_HASH = "c0e8c637c6ea7c97fce7f4d9622aa023"
+BOT_TOKEN = "5542287006:AAHx2XSh1bQWRGevqoc2TmcZ7jBFkyI_d6o"
+
+OXYVER = Client(
+    name = "Pyrogrambot",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN
+)
